9a04cc22fbd19d42286af488a71ae4f76c6ddbce
Rishabh <rishabhntl17@gmail.com>
2018-03-22 20:17:55 +0530

    Edited View
    Added progress Circle

--- GoogleDistanceProject/ViewController.swift
+++ GoogleDistanceProject/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     var subTransitionView = UIView()
+    var progressCircleView = UIView()
+    var progressCircleCenter = CGFloat()
     
     @IBOutlet weak var originView: UIView!
     @IBOutlet weak var transitionView: UIView!
@@ -24,11 +26,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.originView.layer.cornerRadius = self.originView.bounds.width/2.0
-        self.destinationView.layer.cornerRadius = self.destinationView.bounds.width/2.0
-        let frame = transitionView.frame
-        self.subTransitionView = UIView(frame: CGRect(x: 0, y: 0, width: frame.width, height: 0))
-        self.transitionView.addSubview(self.subTransitionView)
+        setFrames()
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,13 +47,31 @@ class ViewController: UIViewController {
 
 extension ViewController
 {
+    func setFrames()
+    {
+        self.originView.layer.cornerRadius = self.originView.bounds.width/2.0
+        self.destinationView.layer.cornerRadius = self.destinationView.bounds.width/2.0
+        let transitionViewFrame = transitionView.frame
+        self.subTransitionView = UIView(frame: CGRect(x: 0 + self.originView.bounds.height/2, y: 0, width: transitionViewFrame.width, height: 0))
+        self.transitionView.addSubview(self.subTransitionView)
+        let originViewFrame = originView.frame
+        self.progressCircleView = UIView(frame: CGRect(x: 0, y: 0, width: originViewFrame.width, height: originViewFrame.height))
+        self.progressCircleView.layer.cornerRadius = self.progressCircleView.bounds.width/2.0
+        self.progressCircleView.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        self.originView.addSubview(progressCircleView)
+        progressCircleCenter = progressCircleView.center.y
+    }
+    
     func animateOnLoad()
     {
-        let frame = transitionView.frame
-        self.subTransitionView.frame = CGRect(x: 0, y: 0, width: frame.width, height: 0)
+        let transitionViewFrame = transitionView.frame
+        self.subTransitionView.frame = CGRect(x: 0, y: 0, width: transitionViewFrame.width, height: 0)
+        self.progressCircleView.center.y = progressCircleCenter
+        self.subTransitionView.layer.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
         UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseIn, animations: {
             self.subTransitionView.layer.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
-            self.subTransitionView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
+            self.subTransitionView.frame = CGRect(x: 0, y: 0, width: transitionViewFrame.width, height: transitionViewFrame.height)
+            self.progressCircleView.frame = CGRect(x: 0, y: self.destinationView.center.y - self.originView.center.y, width: self.progressCircleView.bounds.width, height: self.progressCircleView.bounds.height)
         }, completion : nil)
     }
 
