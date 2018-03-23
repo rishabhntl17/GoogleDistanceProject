93c06b9245a48d9802ebe6400e4413556dbf4d9f
Rishabh <rishabhntl17@gmail.com>
2018-03-23 11:27:22 +0530

    Improvised UI

--- GoogleDistanceProject/ViewController.swift
+++ GoogleDistanceProject/ViewController.swift
@@ -23,10 +23,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var distanceLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var headerDetailLabel: UILabel!
+    @IBOutlet weak var goButtonOutlet: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setFrames()
+        setFramesOnViewLoad()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,9 +37,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.originTextField.center.x  -= self.view.bounds.width
+        self.destinationTextField.center.x += self.view.bounds.width
+        self.goButtonOutlet.center.x += self.view.bounds.width
+    }
+    
     @IBAction func goButtonAction(_ sender: UIButton) {
-        animateOnLoad()
-        Distance().find(origin: originTextField.text!, destination: destinationTextField.text!){ (distance,time) in
+        animateOnGoButtonAction()
+        Distance().find(origin: originTextField.text!.replacingOccurrences(of: " ", with: "+"), destination: destinationTextField.text!.replacingOccurrences(of: " ", with: "+")){ (distance,time) in
             originLabel.text = originTextField.text!
             destinationLabel.text = destinationTextField.text!
             distanceLabel.text = "Total Distance : "+distance
@@ -47,14 +56,21 @@ class ViewController: UIViewController {
 
 extension ViewController
 {
-    func setFrames()
+    func setFramesOnViewLoad()
     {
+        UIView.animate(withDuration: 2, delay: 0, options: .curveEaseIn, animations: {
+            self.headerLabel.alpha = 1.0
+            self.headerDetailLabel.alpha = 1.0
+            self.originTextField.center.x  += self.view.bounds.width
+            self.destinationTextField.center.x -= self.view.bounds.width
+            self.goButtonOutlet.center.x -= self.view.bounds.width
+        }, completion: nil)
         self.originView.layer.cornerRadius = self.originView.bounds.width/2.0
         self.destinationView.layer.cornerRadius = self.destinationView.bounds.width/2.0
         let transitionViewFrame = transitionView.frame
+        let originViewFrame = originView.frame
         self.subTransitionView = UIView(frame: CGRect(x: 0 + self.originView.bounds.height/2, y: 0, width: transitionViewFrame.width, height: 0))
         self.transitionView.addSubview(self.subTransitionView)
-        let originViewFrame = originView.frame
         self.progressCircleView = UIView(frame: CGRect(x: 0, y: 0, width: originViewFrame.width, height: originViewFrame.height))
         self.progressCircleView.layer.cornerRadius = self.progressCircleView.bounds.width/2.0
         self.progressCircleView.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
@@ -62,17 +78,24 @@ extension ViewController
         progressCircleCenter = progressCircleView.center.y
     }
     
-    func animateOnLoad()
+    func animateOnGoButtonAction()
     {
         let transitionViewFrame = transitionView.frame
         self.subTransitionView.frame = CGRect(x: 0, y: 0, width: transitionViewFrame.width, height: 0)
         self.progressCircleView.center.y = progressCircleCenter
         self.subTransitionView.layer.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        self.originLabel.alpha = 0.0
+        self.destinationLabel.alpha = 0.0
+        self.distanceLabel.alpha = 0.0
+        self.timeLabel.alpha = 0.0
         UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseIn, animations: {
+            self.originLabel.alpha = 1.0
+            self.destinationLabel.alpha = 1.0
+            self.distanceLabel.alpha = 1.0
+            self.timeLabel.alpha = 1.0
             self.subTransitionView.layer.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
             self.subTransitionView.frame = CGRect(x: 0, y: 0, width: transitionViewFrame.width, height: transitionViewFrame.height)
             self.progressCircleView.frame = CGRect(x: 0, y: self.destinationView.center.y - self.originView.center.y, width: self.progressCircleView.bounds.width, height: self.progressCircleView.bounds.height)
         }, completion : nil)
     }
-
 }
